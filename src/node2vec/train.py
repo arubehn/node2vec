0e45b530923ad1985e19646346c295f5d7711734
Arne Rubehn <arne@rubehn.com>
2024-06-06 19:27:33 +0200

    restructure

--- src/node2vec/train.py
+++ src/node2vec/train.py
@@ -12,7 +12,7 @@ Knowledge Discovery and Data Mining (KDD), 2016
 import argparse
 import numpy as np
 import networkx as nx
-import node2vec
+import node2vec.graph
 from gensim.models import Word2Vec
 
 
@@ -82,16 +82,16 @@ def read_graph():
     return G
 
 
-def learn_embeddings(walks):
+def learn_embeddings(walks, dimensions=128, window_size=5, workers=8, output="out.emb"):
     """
     Learn embeddings by optimizing the Skipgram objective using SGD.
     """
     walks = [[str(node) for node in walk] for walk in walks]
     # model = Word2Vec(walks, size=args.dimensions, window=args.window_size, min_count=0, sg=1, workers=args.workers,
     # iter=args.iter)
-    model = Word2Vec(walks, vector_size=args.dimensions, window=args.window_size, min_count=0, sg=1,
-                     workers=args.workers)
-    model.wv.save_word2vec_format(args.output)
+    model = Word2Vec(walks, vector_size=dimensions, window=window_size, min_count=0, sg=1,
+                     workers=workers)
+    model.wv.save_word2vec_format(output)
 
     return
 
@@ -101,7 +101,7 @@ def main(args):
     Pipeline for representational learning for all nodes in a graph.
     """
     nx_G = read_graph()
-    G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
+    G = node2vec.graph.Graph(nx_G, args.directed, args.p, args.q)
     G.preprocess_transition_probs()
     walks = G.simulate_walks(args.num_walks, args.walk_length)
     learn_embeddings(walks)
